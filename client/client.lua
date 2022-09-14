d88c8ef193b46f741d9f2b5ee8fc4006d2102859
kibra <62299912+kibradev@users.noreply.github.com>
2022-09-14 21:53:46 +0300

    Add files via upload

--- client/client.lua
+++ client/client.lua
@@ -3,13 +3,31 @@ R.PlayerData = {}
 R.CurrentRequestId = 0
 R.ServerCallbacks = {}
 
+if Config.Framework == "ESX" then
+	ESX = exports['es_extended']:getSharedObject()
+else
+	QBCore = exports['qb-core']:GetCoreObject()
+end
+
 AddEventHandler('0r-core:getSharedObject', function(cb)
 	cb(R)
+end)
+
+exports("Get0RCore", function()
+	return R
+end)
+
+exports("GetFramework", function()
+	return Config.Framework
 end)
 
 function getSharedObject()
 	return R
 end
+
+RegisterNetEvent('qb-spawn:client:openUI', function()
+	TriggerServerEvent('0r-core:onPlayerJoined')
+end)
 
 AddEventHandler('onResourceStart', function(resourceName)
 	if (GetCurrentResourceName() ~= resourceName) then
@@ -22,7 +40,6 @@ RegisterNetEvent("0r-core:setPlayerData")
 AddEventHandler("0r-core:setPlayerData", function(PlayerData)
     R.PlayerData = PlayerData
 end)
-
 
 RegisterNetEvent(Config.events["playerLoaded"][Config.Framework])
 AddEventHandler(Config.events["playerLoaded"][Config.Framework], function()
@@ -41,6 +58,43 @@ AddEventHandler('0r-core:serverCallback', function(requestId, ...)
 	R.ServerCallbacks[requestId] = nil
 end)
 
+R.GetPlayerData = function()
+	if Config.Framework == "ESX" then
+		return ESX.GetPlayerData()
+	else
+		return QBCore.Functions.GetPlayerData()
+	end
+end
+
+
+R.Menu = function()
+	return ESX.UI.Menu
+end
+
+R.DrawText3D = function(x,y,z, text)
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    SetTextScale(0.3, 0.3)
+    SetTextFont(4)
+    SetTextProportional(1)
+    SetTextColour(255, 255, 255, 245)
+
+    SetTextEntry("STRING")
+    SetTextCentre(1)
+    AddTextComponentString(text)
+    DrawText(_x,_y)
+    local factor = (string.len(text)) / 410
+    DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 133)
+end
+
+R.GetIdentifier = function()
+    if Config.Framework == "ESX" then
+        return ESX.GetPlayerData().identifier
+    else
+        return QBCore.Functions.GetPlayerData().citizenid
+    end
+end
+
 R.TriggerServerCallback = function(name, cb, ...)
 	R.ServerCallbacks[R.CurrentRequestId] = cb
 
@@ -53,14 +107,26 @@ R.TriggerServerCallback = function(name, cb, ...)
 	end
 end
 
+R.DrawText2D = function(msg, thisFrame, beep, duration)
+	AddTextEntry('esxHelpNotification', msg)
+
+	if thisFrame then
+		DisplayHelpTextThisFrame('esxHelpNotification', false)
+	else
+		if beep == nil then beep = true end
+		BeginTextCommandDisplayHelp('esxHelpNotification')
+		EndTextCommandDisplayHelp(0, false, beep, duration or -1)
+	end
+end
+
 R.NotifPos = function(pos) 
 	SendNUIMessage({ action = "showNotify", data = { type = "setDefaultPos", pos = pos }  })
 end
 
 
 R.Notif = function(data)
-	SendNUIMessage({ action = "showNotify", data = data })
-end 
+	SendNUIMessage({action = "showNotify", data = data })
+end
  
 RegisterNetEvent("0r-core:notif")
 AddEventHandler("0r-core:notif", R.Notif)
