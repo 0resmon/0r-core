0304e8d7c339a9628a77398c23dd828ba0145f15
kibra <62299912+kibradev@users.noreply.github.com>
2022-09-14 21:54:07 +0300

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,6 @@
+exports("Get0RCore", function()
+	return R
+end)
 
 RegisterNetEvent("0r-core:onPlayerJoined")
 AddEventHandler('0r-core:onPlayerJoined', function()
@@ -17,6 +20,11 @@ AddEventHandler('0r-core:onPlayerJoined', function()
           if Config.Framework == "ESX" then 
              info["firstname"] = data[1].firstname
              info["lastname"] = data[1].lastname
+             info["phone"] = nil
+          else
+               info["firstname"] = info.PlayerData.charinfo.firstname
+               info["lastname"] = info.PlayerData.charinfo.lastname
+               info["phone"] = info.PlayerData.charinfo.phone
           end
           R.Players[source] = info
        end
@@ -38,72 +46,111 @@ AddEventHandler('0r-core:setJob', function(job)
     R.Players[source].job = job
 end)
 
-RegisterCommand("komut", function(source, args)
-   
-   print(R.xPlayer(source).Name())
-end)
-
-
 AddEventHandler('playerDropped', function(reason)
 	local source = source
 	TriggerEvent('0r-core:playerDropped', source, reason)
-  TriggerClientEvent('0r-core:playerDropped', source, source, reason)
+   TriggerClientEvent('0r-core:playerDropped', source, source, reason)
 end)
 
-
-
 R.PrimaryIdentifier = function(source)
-    local identifier = Config.PrimaryIdentifier..':'
-    for _, v in pairs(GetPlayerIdentifiers(source)) do
-        if string.match(v, identifier) then
-            identifier = string.gsub(v, identifier, '')
-            return identifier
-        end
-    end
+   local identifier = Config.PrimaryIdentifier..':'
+   for _, v in pairs(GetPlayerIdentifiers(source)) do
+      if string.match(v, identifier) then
+         identifier = string.gsub(v, identifier, '')
+         return identifier
+      end
+   end
 end
 
 R.xPlayer = function(source)
-  
-  local source = source
- 
-  if Config.Framework == "ESX" then 
-	   rPlayer = ESX.GetPlayerFromId(source)
-  elseif Config.Framework == "QBCore" then 
-	   rPlayer = QBCore.Functions.GetPlayer(source)
-  end
- 
-  rPlayer = R.RemapPlayer(rPlayer)
-  
-  return rPlayer
+   local source = source
+   if Config.Framework == "ESX" then 
+      rPlayer = ESX.GetPlayerFromId(source)
+   elseif Config.Framework == "QBCore" then 
+      rPlayer = QBCore.Functions.GetPlayer(source)
+      if not rPlayer then return end
+      rPlayer.identifier = rPlayer.PlayerData.citizenid
+      rPlayer.source = rPlayer.PlayerData.source
+   end
+   rPlayer = R.RemapPlayer(rPlayer)
+   return rPlayer
+end
+
+R.UsableItem = function(item, cb)
+   if Config.Framework == "ESX" then
+      return ESX.RegisterUsableItem(item, cb)
+   else
+      return QBCore.Functions.CreateUseableItem(item, cb)
+   end
+end
+
+R.GetItemBySlot = function(source, slot)
+   if Config.Framework == "QBCore" then
+      local Player = QBCore.Functions.GetPlayer(source)
+      slot = tonumber(slot)
+      return Player.PlayerData.items[slot]
+   else
+      local Player = ESX.GetPlayerFromId(source)
+      slot = tonumber(slot)
+      return Player.items[slot]
+   end
 end
 
 R.GetIdentifier = function(source)
-  if Config.Framework == "ESX" then 
-    return ESX.GetPlayerFromId(source).identifier
-  elseif Config.Framework == "QBCore" then 
-    return QBCore.Functions.GetPlayer(source).PlayerData.citizenid
-  else 
-    return R.PrimaryIdentifier(source)
-  end
+   if Config.Framework == "ESX" then 
+      return ESX.GetPlayerFromId(source).identifier
+   elseif Config.Framework == "QBCore" then 
+      return QBCore.Functions.GetPlayer(source).PlayerData.citizenid
+   else 
+      return R.PrimaryIdentifier(source)
+   end
 end
- 
 
 R.RemapPlayer = function(xPlayer)
    local self = {}
    
    self.GetAccountData = function(account)
       if Config.Framework == "ESX" then 
-         return xPlayer.getAccount(account)
+         if account == "cash" then account = "money" end
+         return xPlayer.getAccount(account).money
       else 
          return xPlayer.PlayerData.money[account]
       end
    end
 
+   self.GetProfile = function(xx)
+      if Config.Framework == "ESX" then 
+         query = "SELECT * FROM `users` WHERE identifier = '"..xPlayer.identifier.."' "
+         result = R.ExecuteSql(query)
+         if xx == "name" then return result[1].firstname..' '..result[1].lastname else return nil end
+      else 
+         if xx == "name" then return xPlayer.PlayerData.charinfo.firstname..' '..xPlayer.PlayerData.charinfo.lastname else return xPlayer.PlayerData.charinfo.phone end
+      end
+   end
+
+   self.AddItem = function(item, amount, slot, metadata)
+      if Config.Framework == "ESX" then 
+         return xPlayer.addInventoryItem(item, amount, slot, metadata)
+      else 
+         return xPlayer.Functions.AddItem(item, amount, slot, metadata)
+      end
+   end
+
    self.GiveAccountMoney = function(account, money)
       if Config.Framework == "ESX" then 
+         if account == "cash" then account = "money" end
          xPlayer.addAccountMoney(account, money)
       else 
          xPlayer.Functions.AddMoney(account, money)
+      end
+   end
+
+   self.RemoveMoney = function(account, money)
+      if Config.Framework == "ESX" then 
+         if account == "cash" then account = "money" end
+         return xPlayer.removeAccountMoney(account, money)
+      else 
+         return xPlayer.Functions.RemoveMoney(account, money)
       end
    end
 
@@ -127,7 +174,9 @@ R.RemapPlayer = function(xPlayer)
       if Config.Framework == "ESX" then 
           return xPlayer.getInventoryItem(item)
       else 
-         return xPlayer.Functions.GetItemByName()
+         x = xPlayer.Functions.GetItemByName(item)
+         x.count = x.amount
+         return x
       end  
    end
 
@@ -138,19 +187,19 @@ R.RemapPlayer = function(xPlayer)
          TriggerClientEvent("0r-core:notif", xPlayer.PlayerData.source, { type = type or "success", text = text, icon = icon or "💬" })
       end
    end
- 
+
    return R.MergeTable(self, xPlayer)
 end
 
 R.MergeTable = function(t1, t2)
-  for k, v in pairs(t2) do
-      if (type(v) == "table") and (type(t1[k] or false) == "table") then
-          R.MergeTable(t1[k], t2[k])
-      else
-          t1[k] = v
-      end
-  end
-  return t1
+   for k, v in pairs(t2) do
+         if (type(v) == "table") and (type(t1[k] or false) == "table") then
+            R.MergeTable(t1[k], t2[k])
+         else
+            t1[k] = v
+         end
+   end
+   return t1
 end
 
 R.RegisterServerCallback = function(name, cb)
